6c5255e238db86cf3f3000cab359e6f66c186fc5
Anirban <anirban.s@finlabsindia.com>
2023-04-23 23:31:37 +0530

    login and reg fine tuning

--- app/src/main/java/com/example/policemitra/login.java
+++ app/src/main/java/com/example/policemitra/login.java
@@ -2,13 +2,27 @@ package com.example.policemitra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
+
+    TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        signup = findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this,registration.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/policemitra/registration.java
+++ app/src/main/java/com/example/policemitra/registration.java
@@ -2,13 +2,26 @@ package com.example.policemitra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class registration extends AppCompatActivity {
 
+    TextView signin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        getSupportActionBar().hide();
+        signin = findViewById(R.id.signin);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(registration.this,login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
