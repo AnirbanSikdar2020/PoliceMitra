82e6d3e1e86182876edad094486050b0a94a240b
Anirban <anirban.s@finlabsindia.com>
2023-04-24 22:10:11 +0530

    committed

--- app/src/main/java/com/example/policemitra/home.java
+++ app/src/main/java/com/example/policemitra/home.java
@@ -2,27 +2,36 @@ package com.example.policemitra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class home extends AppCompatActivity {
-
-    Button back;
-
+    ViewFlipper slider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        back = findViewById(R.id.back);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(home.this,login.class);
-                startActivity(intent);
-            }
-        });
+
+
+        int sliderArray[] = {R.drawable.slide2,R.drawable.slide1};
+        slider=(ViewFlipper)findViewById(R.id.slider);
+
+        for(int i=0;i<sliderArray.length;i++)
+        {
+            showImage(sliderArray[i]);
+        }
+    }
+
+    public void showImage(int img)
+    {
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(img);
+        slider.addView(imageView);
+        slider.setFlipInterval(3000);
+        slider.setAutoStart(true);
+        slider.setInAnimation(this, android.R.anim.slide_in_left);
+        slider.setOutAnimation(this, android.R.anim.slide_out_right);
+
     }
 }
