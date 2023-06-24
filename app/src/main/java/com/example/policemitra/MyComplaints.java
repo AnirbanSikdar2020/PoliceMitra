8e2200208cfb187d05475dfa75660e845dd4c6c6
Anirban <anirban.s@finlabsindia.com>
2023-06-24 22:21:14 +0530

    My complaint

---
+++ app/src/main/java/com/example/policemitra/MyComplaints.java
@@ -0,0 +1,14 @@
+package com.example.policemitra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyComplaints extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_complaints);
+    }
+}
