fc3b5997105d4a1a4a424995e9d634bd04f892c1
Anirban <anirban.s@finlabsindia.com>
2023-06-25 21:49:05 +0530

    Complete

--- app/src/main/java/com/example/policemitra/home.java
+++ app/src/main/java/com/example/policemitra/home.java
@@ -1,5 +1,7 @@
 package com.example.policemitra;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,6 +15,7 @@ import android.widget.ViewFlipper;
 public class home extends Fragment {
 
     ViewFlipper slider;
+    ImageView constitution,wbpnotify,kpnotify;
     public home() {
         // Required empty public constructor
     }
@@ -29,12 +32,43 @@ public class home extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         int sliderArray[] = {R.drawable.pic_1,R.drawable.police_1,R.drawable.police_2,R.drawable.police_4, R.drawable.police_6,R.drawable.police_9,R.drawable.police_10, R.drawable.pic_2,R.drawable.pic_3};
         slider=view.findViewById(R.id.slider);
+        constitution = view.findViewById(R.id.constitution);
+        wbpnotify = view.findViewById(R.id.wbpnotify);
+        kpnotify = view.findViewById(R.id.kpnotify);
+
 
         for(int i=0;i<sliderArray.length;i++)
         {
             showImage(sliderArray[i]);
         }
-        // Inflate the layout for this fragment
+
+        constitution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.indiacode.nic.in/bitstream/123456789/15240/1/constitution_of_india.pdf";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        kpnotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.kolkatapolice.gov.in/contactus.aspx";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        wbpnotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://wbpolice.gov.in/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
         return view;
     }
 
